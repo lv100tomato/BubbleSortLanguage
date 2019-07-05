a1a028ca2eb1f91b2d349690c759f3ef57c35ef5
lv100 <36735561+lv100tomato@users.noreply.github.com>
2019-07-05 10:44:08 +0900

    Update v1.0

--- Program.cs
+++ Program.cs
@@ -15,10 +15,15 @@ namespace BubbleSortLanguage
         public static int PointerB;
         public static int Index;
 
+        public static int MemSizeHalf = 2048;
+        public static int Mod = 50;
+
         static void Main(string[] args)
         {
             List<long> preSource = new List<long>();
             string read;
+            bool debug = false;
+            string debOut = "";
 
             //read source
             if(args.Length < 1)
@@ -69,12 +74,20 @@ namespace BubbleSortLanguage
                 }
 
                 Source = preSource.ToArray();
+
+                if(args.Length > 1)
+                {
+                    if(args[1] == "-debug")
+                    {
+                        debug = true;
+                    }
+                }
             }
 
             //bubblesort
-            Memory = Enumerable.Repeat(0, 2048).ToArray();
-            PointerA = 0;
-            PointerB = 0;
+            Memory = Enumerable.Repeat(0, MemSizeHalf * 2).ToArray();
+            PointerA = MemSizeHalf;
+            PointerB = MemSizeHalf;
             Index = 0;
 
             bool isSorted;
@@ -92,63 +105,139 @@ namespace BubbleSortLanguage
                         Source[Index + 1] = Source[Index];
                         Source[Index] = cmd;
 
+                        //output command
+                        if (debug)
+                        {
+                            Console.WriteLine("----------------");
+                            Console.WriteLine(" Index" + "    " + "          SourceCode");
+                            Console.WriteLine("");
+                            Console.WriteLine(LongToString(Index - 1, 6) + " :  " + ArrayToString(Source, Index - 1));
+                            Console.WriteLine(LongToString(Index, 6) + " :* " + ArrayToString(Source, Index));
+                            Console.WriteLine(LongToString(Index + 1, 6) + " :* " + ArrayToString(Source, Index + 1));
+                            Console.WriteLine(LongToString(Index + 2, 6) + " :  " + ArrayToString(Source, Index + 2));
+                        }
+
                         //do command
-                        cmd = cmd % 32;
-                        cmd = cmd + (Source[Index + 1] % 32);
-                        cmd = cmd % 32;
+                        cmd = cmd % Mod;
+                        cmd = cmd + (Source[Index + 1] % Mod);
+                        cmd = cmd % Mod;
+
+                        if (debug)
+                        {
+                            Console.WriteLine("--------");
+                            Console.WriteLine("Command : " + cmd);
+                        }
+
                         switch (cmd)
                         {
                             case 0:
                                 PointerA++;
+                                if (debug) Console.WriteLine("PointerA : " + (PointerA - MemSizeHalf));
                                 break;
                             case 1:
                                 PointerB++;
+                                if (debug) Console.WriteLine("PointerB : " + (PointerB - MemSizeHalf));
                                 break;
                             case 2:
                                 PointerA--;
+                                if (debug) Console.WriteLine("PointerA : " + (PointerA - MemSizeHalf));
                                 break;
                             case 3:
                                 PointerB--;
+                                if (debug) Console.WriteLine("PointerB : " + (PointerB - MemSizeHalf));
                                 break;
                             case 4:
                                 Memory[PointerA]++;
+                                if (debug) Console.WriteLine("(*PointerA) : " + Memory[PointerA]);
                                 break;
                             case 5:
                                 Memory[PointerB]++;
+                                if (debug) Console.WriteLine("(*PointerB) : " + Memory[PointerB]);
                                 break;
                             case 6:
                                 Memory[PointerA]--;
+                                if (debug) Console.WriteLine("(*PointerA) : " + Memory[PointerA]);
                                 break;
                             case 7:
                                 Memory[PointerB]--;
+                                if (debug) Console.WriteLine("(*PointerB) : " + Memory[PointerB]);
                                 break;
                             case 8:
+                                if (debug)
+                                {
+                                    Console.Write("Output : " + debOut);
+                                    debOut = debOut + ((char)Memory[PointerA]).ToString();
+                                }
                                 Console.Write((char)Memory[PointerA]);
+                                if (debug) Console.WriteLine();
                                 break;
                             case 9:
+                                if (debug)
+                                {
+                                    Console.Write("Output : " + debOut);
+                                    debOut = debOut + ((char)Memory[PointerB]).ToString();
+                                }
                                 Console.Write((char)Memory[PointerB]);
+                                if (debug) Console.WriteLine();
                                 break;
                             case 10:
+                                if (debug) Console.Write("Load to (*PointerA) : ");
                                 Memory[PointerA] = Console.Read();
                                 break;
                             case 11:
+                                if (debug) Console.Write("Load to (*PointerB) : ");
                                 Memory[PointerB] = Console.Read();
                                 break;
                             case 12:
                                 Source[Index + Memory[PointerA]] += Memory[PointerB];
+                                if (debug) Console.WriteLine("SourceCode Line " + (Index + Memory[PointerA]) + " : " + Source[Index + Memory[PointerA]] + " (" + (Memory[PointerB] < 0 ? "" : "+") + Memory[PointerB] + ")");
                                 break;
                             case 13:
                                 Source[Index + Memory[PointerA]] -= Memory[PointerB];
+                                if (debug) Console.WriteLine("SourceCode Line " + (Index + Memory[PointerA]) + " : " + Source[Index + Memory[PointerA]] + " (" + (Memory[PointerB] > 0 ? "" : "+") + (-Memory[PointerB]) + ")");
+                                break;
+                            case 14:
+                                PointerA += (Memory[PointerB] == 0 ? 1 : 0);
+                                if (debug) Console.WriteLine("(*PointerB) = " + Memory[PointerB] + " -> PointerA : " + (PointerA - MemSizeHalf));
+                                break;
+                            case 15:
+                                PointerA -= (Memory[PointerB] == 0 ? 1 : 0);
+                                if (debug) Console.WriteLine("(*PointerB) = " + Memory[PointerB] + " -> PointerA : " + (PointerA - MemSizeHalf));
                                 break;
                             default:
                                 break;
+                        }
+
+                        if (debug)
+                        {
+                            Console.ReadLine();
                         }
                     }
                 }
 
             } while (!isSorted);
-            Console.WriteLine();
+            //Console.WriteLine();
             //Console.ReadLine();
+        }
+
+        private static string ArrayToString(long[] array, int ind)
+        {
+            if(ind < 0 || ind >= array.Length)
+            {
+                return "";
+            }
+            else
+            {
+                string output = LongToString(array[ind], 20);
+                return output;
+            }
+        }
+
+        private static string LongToString(long num, int keta)
+        {
+            string output = num.ToString("D");
+            for (; output.Length < keta; output = " " + output) ;
+            return output;
         }
     }
 }
